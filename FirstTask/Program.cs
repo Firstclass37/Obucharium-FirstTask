66a5ea4ad1c7f5c70d23816d74d640a0f9f825e5
Ivan <firstclass37@mail.ru>
2016-05-02 20:53:32 +0300

    FirstTask

--- FirstTask/Program.cs
+++ FirstTask/Program.cs
@@ -10,6 +10,41 @@ namespace FirstTask
     {
         static void Main(string[] args)
         {
+            List<String> ourGroup = new List<string>()
+            {
+                "Андрей П.",
+                "Константин",
+                "Андрей Ж.",
+                "Виталий",
+                "Иван",
+                "Илья"
+            };
+
+            Mix(ourGroup);
+            Show(ourGroup);
+            Console.ReadKey();
+
         }
+
+        static void Mix(List<string> group)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int x = rand.Next(0, group.Count - 1);
+                int y = rand.Next(0, group.Count - 1);
+                string temp = group[x];
+                group[x] = group[y];
+                group[y] = temp; 
+            }
+        }
+
+        static void Show(List<string> group)
+        {
+            foreach (string item in group)
+                Console.WriteLine(item);
+        }
+
+
     }
 }
